2d91763ab096bba9196f4d5d5ad3775fb13bca0e
Victoria Marsicovetere <vimarsicovetere@davidson.edu>
2021-06-17 16:06:40 -0400

    non-random trying to get game over to display when wrong button is clicked

--- site/buttons.js
+++ site/buttons.js
@@ -17,3 +17,33 @@ let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('click', function() {
   document.querySelector('.blue').style.backgroundColor = 'skyblue';
 });
+
+// const nonRandom = ["red", "blue", "blue", "green"];
+alert('red')
+let afterAlert = document.querySelector('.simon-button.red');
+let afterAlert2 = document.getElementById('tapBlue');
+// let afterAlert2 = document.querySelector('.simon-button.blue');
+// let afterAlert3 = document.querySelector('.simon-button.green');
+
+afterAlert.addEventListener("click", () => {
+ 	alert('blue');
+   if (afterAlert2.clicked){
+     alert('GAME OVER!');
+   }
+  });
+// });
+
+// afterAlert.addEventListener('click', clickHandler);
+
+// let redState = false;
+// let blueState = false;
+// let greenState = false;
+// function clickHandler(){
+//   if(afterAlert.clicked == true){
+//     redState = true;
+//     alert('blue');
+//   }
+//   else{
+//     alert('GAME OVER!');
+//   }
+// }
