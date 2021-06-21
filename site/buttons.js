4e9dd2f015b3b51b26ee338e4833ac86f4895299
Victoria Marsicovetere <vimarsicovetere@davidson.edu>
2021-06-21 12:55:12 -0400

    game

--- site/buttons.js
+++ site/buttons.js
@@ -1,11 +1,29 @@
 let playerOrder = [];
+let order = ['red', 'red', 'blue', 'green', 'red', 'red', 'green', 'blue'];
+let playerStep = 0;
+let seconds = 0;
+let el = document.querySelector('.timeDisplay');
+
 $('.simon-button.green').on('mousedown', function () {
   document.querySelector('.simon-button.green').style.filter = 'brightness(150%)';
 });
 
 $('.simon-button.green').on('mouseup', function () {
   document.querySelector('.simon-button.green').style.filter = 'brightness(100%)';
-  playerOrder.push('green')
+  if(order[playerStep] == 'green') {
+    playerOrder.push('green');
+    playerStep += 1;
+    console.log("Guess correct");
+    if(order.length == playerOrder.length){
+      alert("Right length");
+    }  
+    if(playerOrder.length == order.length) {
+      alert('It took you ' + seconds + ' seconds to win!');
+    } 
+  }
+   else {
+    console.log("GAME OVER!");
+   }
 });
 
 $('.simon-button.red').on('mousedown', function () {
@@ -14,7 +32,21 @@ $('.simon-button.red').on('mousedown', function () {
 
 $('.simon-button.red').on('mouseup', function () {
   document.querySelector('.simon-button.red').style.filter = 'brightness(100%)';
-  playerOrder.push('red')
+  if(order[playerStep] == 'red') {
+    playerOrder.push('red');
+    playerStep += 1;
+    console.log("Guess correct");
+    if(order.length == playerOrder.length){
+      console.log("Right length");
+    }
+    if(playerOrder.length == order.length) {
+      alert('It took you ' + seconds + ' seconds to win!');
+    }   
+  }
+   else {
+    alert("GAME OVER!");
+   }
+
 });
 
 $('.simon-button.yellow').on('mousedown', function () {
@@ -23,7 +55,21 @@ $('.simon-button.yellow').on('mousedown', function () {
 
 $('.simon-button.yellow').on('mouseup', function () {
   document.querySelector('.simon-button.yellow').style.filter = 'brightness(100%)';
-  playerOrder.push('yellow')
+  if(order[playerStep] == 'yellow') {
+    playerOrder.push('yellow');
+    playerStep += 1;
+    console.log("Guess correct");
+    if(order.length == playerOrder.length){
+      console.log("Right length");
+    }
+    if(playerOrder.length == order.length) {
+      alert('It took you ' + seconds + ' seconds to win!');
+    } 
+  }
+   else {
+    alert("GAME OVER!");
+   }
+
 });
 
 $('.simon-button.blue').on('mousedown', function () {
@@ -32,99 +78,36 @@ $('.simon-button.blue').on('mousedown', function () {
 
 $('.simon-button.blue').on('mouseup', function () {
   document.querySelector('.simon-button.blue').style.filter = 'brightness(70%)';
-  playerOrder.push('blue')
+  if(order[playerStep] == 'blue') {
+    playerOrder.push('blue');
+    playerStep += 1;
+    console.log("Guess correct");
+    if(order.length == playerOrder.length){
+      console.log("Right length");
+    }  
+    if(playerOrder.length == order.length) {
+      alert('It took you ' + seconds + ' seconds to win!');
+    } 
+  }
+   else {
+    alert("GAME OVER!");
+   }
+
 });
 
 //This displays the the time for how long it took the user to complete the pattern 
 //The user wants to beat their previous time each round
-
-let seconds = 0;
-let el = document.querySelector('.timeDisplay');
 
 function incrementSeconds() {
   seconds += 1;
   el.innerHTML = seconds;
 }
 
-setInterval(incrementSeconds, 1000);
+document.getElementById('start').addEventListener('click', function (event) {
+  event.preventDefault();
+  setInterval(incrementSeconds, 1000);
+});
 
-let order = ['red', 'red', 'blue', 'green', 'red', 'red', 'green', 'blue'];
-
-let equals = (order, playerOrder) => JSON.stringify(order) === JSON.stringify(playerOrder);
-if(equals(order, playerOrder)) {
-  alert("Good job! It took you " + seconds + " seconds!");
-}
- else {
-  alert("GAME OVER!");
- }
- console.log(playerOrder);
-
-
-
-// let greenButton = document.querySelector('.simon-button.green');
-// greenButton.addEventListener('click', function() {
-//   document.querySelector('.green').style.backgroundColor = 'lawngreen';
-//   userClicks.push('green');
-// });
-
-// let redButton = document.querySelector('.simon-button.red');
-// redButton.addEventListener('click', function() {
-//   document.querySelector('.red').style.backgroundColor = 'pink';
-//   userClicks.push('red');
-// });
-
-// let yellowButton = document.querySelector('.simon-button.yellow');
-// yellowButton.addEventListener('click', function() {
-//   document.querySelector('.yellow').style.backgroundColor = 'lemonchiffon';
-//   userClicks.push('yellow');
-// });
-
-// let blueButton = document.querySelector('.simon-button.blue');
-// blueButton.addEventListener('click', function() {
-//   document.querySelector('.blue').style.backgroundColor = 'lightblue';
-//   userClicks.push('blue');
-// });
-
-
-// let prevSequence = [];
-
-// let buttonPresses = 0;
-
-// function generateSeq() {
-//   let tiles = ['red', 'green', 'blue', 'yellow'];
-//   let randomColor = tiles[Math.floor(Math.random() * tiles.length)]; //returns index of random color in tile 
-
-//   return randomColor;
-// }
-
-// function nextColor() {
-//   prevSequence.push(generateSeq());
-// }
-
-// let compareSeqs = (prevSequence, userClicks) =>
-//   prevSequence.length === userClicks.length &&
-//   prevSequence.every((v, i) => v === userClicks[i]);
-
-//   console.log(compareSeqs(prevSequence, userClicks));
-//   console.log(userClicks);
-
-// if (compareSeqs){
-//   nextColor();
-// }
-// else{
-//   alert('GAME OVER');
-// }
-
-//list1 - empty list to append the user color that is clicked
-//to add nameoflist.push
-//list2 - list that has previous sequence 
-//random color generator add to list2 
-//click from user adds to list1 
-
-//compare both list1 and list2, if == 
-  //update the buttonPresses +1
-  //add next color using random generator
-  //else
-    //lose
-    //display buttonPress
-    //buttonPress reset to 0
+document.getElementById('reset').addEventListener('click', function (event) {
+  <a href="file:///C:/Users/torim/OneDrive/Documents/Liberal%20Arts%20in%20Silicon%20Valley%20Summer%202021/project-js-simon/site/index.html"></a>;
+});
